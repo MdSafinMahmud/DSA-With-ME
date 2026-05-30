26e2c2ea74b94eb2097e90b64d36759af09aded3
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-30 23:29:05 +0600

    python

---
+++ 0.Function/WithPerameters.py
@@ -0,0 +1,5 @@
+def function(name):
+    print("Hello World!!", name)
+
+
+function("Safin")
---
+++ 0.Function/WithReturnValue.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+
+print("Enter two numbers to add")
+result = add(int(input()), int(input()))
+print(result)
