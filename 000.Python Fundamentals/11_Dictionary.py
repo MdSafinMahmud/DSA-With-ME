0f0916e83971fde60e0ff47a8e8d9d629be58dfe
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-18 20:30:50 +0600

    Update 11_Dictionary.py

--- 000.Python Fundamentals/11_Dictionary.py
+++ 000.Python Fundamentals/11_Dictionary.py
@@ -1,9 +1,15 @@
-a={
-    "key": "value",
-    "harry": "code",
-    "marks": "100",
-    "list": [1, 2, 9]
-}
-print(a["key"]) # Output: "value"
-print(a["list"]) # Output: [1, 2, 9]
+a = {"key": "value", "harry": "code", "marks": "100", "list": [1, 2, 9]}
+print(a["key"])  # Output: "value"
+print(a["list"])  # Output: [1, 2, 9]
 print(a["marks"])
+# methods
+a.items()
+print(a.items())
+print(a)
+print(a.keys())
+print(a)
+# provide a valid value for the new key
+a.update({"friend": "safin"})
+print(a)
+a.get("name")
+print(a)
