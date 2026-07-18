3b3e7d3e3c0bfb16191f97fae41180b68ac0a4ab
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-18 19:03:23 +0600

    dsa

---
+++ 000.Python Fundamentals/06_FunctionPart2.py
@@ -0,0 +1,8 @@
+# Example And Syntax Of A Function
+def func1():
+    print("Hey")
+func1()
+# Types Of Functions In Python
+# There are two types of functions in python:
+# Built in functions (Already present in python)
+# User defined functions (Defined by the user)
---
+++ 000.Python Fundamentals/07_FunctionsWithArguments.py
@@ -0,0 +1,12 @@
+def greet(name):
+    gr = "Hello " + name
+    return gr
+
+
+a = greet ("Safin")
+print (a)
+
+def greet (name2="Stranger"):
+   print(f"Hello,{name2}")
+greet ()
+greet("Safin")
