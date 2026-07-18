a0c6773c8658b6bd4dcd7fe6f70f69b7a37b8c08
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-18 18:50:50 +0600

    dsa

---
+++ 000.Python Fundamentals/05_Function.py
@@ -0,0 +1,13 @@
+# funtion
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+d = int(input("Enter a number: "))
+sum = a + b + c + d
+difference = a - b - c - d
+print("1st number:", a)
+print("2nd number:", b)
+print("3rd number:", c)
+print("4th number:", d)
+print("Sum: ", sum)
+print("Diffrence: ", difference)
